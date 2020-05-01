3d17d2a0e15909095a2c829eaeea3e56d259b70b
pankajbarhate13 <pankajbarhate13@gmail.com>
2020-05-01 18:25:22 +0530

    Add Todo list

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, {Fragment, useState, FormEventHandler} from 'react'
 import ReactDOM from 'react-dom'
 
+interface ITodo{
+  text: string
+  complete: boolean
+}
 
+ 
+
+type FormElem =  React.FormEvent<HTMLFormElement>
 export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>('')
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+
+  const handleSubmit = (e:FormElem):void =>{ 
+    e.preventDefault()
+    addTodo(value)
+    setValue('')
+  }
+    
+  const addTodo = ( text:string)=>{
+    const newTodos: ITodo[]=[...todos, {text, complete:false}]
+    setTodos(newTodos)
+  }
+  const completeTodo = (index:number):void =>{
+    const newTodos: ITodo[]=[...todos]
+    newTodos[index].complete = !newTodos[index].complete
+    setTodos(newTodos)
+  }
+
+  const removeTodo = (index:number):void=>{
+    const newTodos: ITodo[]=[...todos]
+      newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+  
+  console.log(todos)
   return (
-    <h1>
-      Hello!!
-    </h1>
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange= {e => setValue(e.target.value)} required/>
+        <button type="submit">Add Todo</button>
+      </form>
+      <section>
+        {todos.map((todo:ITodo, index:number)=>
+          <Fragment key={index}>
+            <div style={{textDecoration: todo.complete ? 'line-through': ''}}>{todo.text}</div>
+            <button type="button" onClick={()=> completeTodo(index)}>{todo.complete ? 'Incomplete' : 'Complete'}</button>
+            <button type="submit" onClick={()=> removeTodo(index)}>&times;</button>
+          </Fragment>
+        )}
+      </section>
+    </Fragment>
   )
 }
 const root = document.getElementById('app-root')
